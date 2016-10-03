d2c44cf4dfbe29ebf59d68bd639b8139ee056d15
Albert Pető <petoalbert32@gmail.com>
2016-10-03 17:00:23 +0200

    Organize code into functions

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
+init();
+render();
 
+var scene, camera, renderer;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75,
-                                         window.innerWidth/window.innerHeight,
-                                         0.1,
-                                         1000);
-var renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75,
+                                          window.innerWidth/window.innerHeight,
+                                          0.1,
+                                          1000);
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-camera.position.z += 6;
+  camera.position.z += 6;
+
+  var loader = new THREE.OBJLoader();
+
+  loader.load('grass.obj',loadGrass);
+}
 
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene,camera);
 }
 
-render();
 
 function loadGrass(object) {
   var vertices = object.children[0].geometry.getAttribute("position");
@@ -32,7 +40,3 @@ function loadGrass(object) {
   var mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 }
-
-var loader = new THREE.OBJLoader();
-
-loader.load('grass.obj',loadGrass);
