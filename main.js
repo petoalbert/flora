ac7bc0ab67604120c1c27ee4a9e93240d759b794
Albert Pető <petoalbert32@gmail.com>
2016-10-03 16:56:57 +0200

    Add grass from blender-generated object file

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
+
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75,
                                          window.innerWidth/window.innerHeight,
                                          0.1,
                                          1000);
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BufferGeometry();
-
-var vertices = new THREE.BufferAttribute(new Float32Array(9), 3);
-vertices.setXYZ(0, -1, -1, 0);
-vertices.setXYZ(1, 1, -1, 0);
-vertices.setXYZ(2, 0, 1, 0);
-geometry.addAttribute('position', vertices);
-
-var material = new THREE.RawShaderMaterial( {
-  vertexShader: document.getElementById('vertexShader').textContent,
-  fragmentShader: document.getElementById('fragmentShader').textContent,
-  side: THREE.DoubleSide,
-});
-
-console.log("Content!!!");
-console.log(document.getElementById('vertexShader').textContent);
-
-var mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-camera.position.z += 3;
+camera.position.z += 6;
 
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene,camera);
 }
+
 render();
+
+function loadGrass(object) {
+  var vertices = object.children[0].geometry.getAttribute("position");
+  var geometry = new THREE.BufferGeometry();
+  geometry.addAttribute('position',vertices);
+
+  var material = new THREE.RawShaderMaterial( {
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    side: THREE.DoubleSide,
+  });
+
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+}
+
+var loader = new THREE.OBJLoader();
+
+loader.load('grass.obj',loadGrass);
